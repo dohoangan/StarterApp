3249774eb7f37c7e2bb2f127e04872e45cc3f872
An D. Hoang <an@izproperty.com>
2014-02-27 13:58:53 +0700

    Starter App Templete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Probases::Application.config.secret_key_base = 'b106188f3c5b4dc81857fc3831446b477ef14a2697b54c8417c0ac64409d62c725b96ce2c1311ac256e3b094bd6739e4d38809934afd97365d941e345a8f79a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Probases::Application.config.session_store :cookie_store, key: '_probases_session'
